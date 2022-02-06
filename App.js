0e1f7285e666498502e06b13444a235247bddacc
Elozino A. Ovedhe <ovedhee@gmail.com>
2022-02-06 12:59:47 +0100

    redux fixed

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Main from "./Main";
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-// import store from "./src/store";
-// import CustomTabNavigator from "./src/components/CustomTabNavigator";
 
 export default function App() {
   return (
+    <Provider store={store}>
       <Main />
-    // <Provider>
-    // </Provider>
+    </Provider>
   );
 }
--- components/NavTab.jsx
+++ components/NavTab.jsx
@@ -1,46 +1,51 @@
 import { StyleSheet, View, TouchableOpacity } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { COLORS } from "../constants/colors";
+import { useSelector, useDispatch } from "react-redux";
+import { tabActive } from "../redux/actions/navigationAction";
 
 const NavTab = ({ navigation }) => {
-  const [categoryIndex, setCategoryIndex] = useState(0);
+  const { activeIndex, focused } = useSelector((state) => state.navigationReducer);
+  const dispatch = useDispatch();
 
-  function color(val) {
-    setCategoryIndex(val);
-    console.log(categoryIndex);
-  }
+
   return (
     <View style={styles.navTab}>
       <TouchableOpacity
+        activeOpacity={0.8}
         onPress={() => {
           navigation.navigate("Home");
-          color(1);
+          dispatch(tabActive(1));
+          console.log(dispatch(tabActive(1)));
         }}
       >
         <Ionicons
           name="ios-home"
           size={24}
-          color={categoryIndex === 1 ? "green" : "orange"}
+          color={activeIndex === 1 ? "green" : "black"}
         />
       </TouchableOpacity>
-      <View>
+
+      <TouchableOpacity activeOpacity={0.5}>
         <Ionicons name="md-bookmark-outline" size={24} color="black" />
-      </View>
-      <View>
+      </TouchableOpacity>
+
+      <TouchableOpacity activeOpacity={0.5}>
         <Ionicons name="ios-notifications-outline" size={24} color="black" />
-      </View>
+      </TouchableOpacity>
+
       <TouchableOpacity
         activeOpacity={0.8}
         onPress={() => {
           navigation.navigate("Profile");
-          color(2);
+          dispatch(tabActive(2));
         }}
       >
         <Ionicons
           name="md-person-outline"
           size={24}
-          color={categoryIndex === 2 ? "green" : "orange"}
+          color={activeIndex === 2 ? "green" : "black"}
         />
       </TouchableOpacity>
     </View>
